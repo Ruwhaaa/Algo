3a9659708d042160db4c9df5b409be2a7afb3d36
Enzo Tessonnieres <enzo.tessonnieres@outlook.fr>
2021-03-19 15:58:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var temp = 1;
-var valeur_carte = 1
+var valeur_carte = 15
 let tableau_carte = [];
+let tableau_mise = [];
 var index = 0;
 var token = 0;
 var nbjoueurs = 0;
+var mise = 0;
 
 while (valeur_carte <= 13) {
     while (token != 24) {
@@ -15,11 +17,21 @@ while (valeur_carte <= 13) {
     valeur_carte++;
     token = 0;
 }
-setTimeout(() => {  
-    nbjoueurs = window.prompt('Nombre de joueurs de 1 à 7 : ','ex : 5');
-    while (nbjoueurs > 7 || nbjoueurs < 1) {
-        nbjoueurs = window.prompt('rentrez à nouveau un noubre de joueur de 1 à 7 : ','ex : 5');
+
+nbjoueurs = window.prompt('Nombre de joueurs de 1 à 7 : ', 'ex : 5');
+while (nbjoueurs > 7 || nbjoueurs < 1) {
+    nbjoueurs = window.prompt('rentrez à nouveau un noubre de joueur de 1 à 7 : ', 'ex : 5');
+}
+while (temp <= nbjoueurs) {
+    mise = window.prompt(`joueur ${temp} à votre tour de miser : `, 'ex : 20');
+    while (mise < 5 || mise > 300) {
+        mise = window.prompt(`joueur ${temp} à votre tour de miser : `, 'ex : 20');
+        }
+    if (temp == 1) {
+        nbjoueurs++;
     }
-    console.log(nbjoueurs);
-}, 2000);
+    tableau_mise[temp] = mise;
+    temp++;
+}
+console.log("hello");
 
